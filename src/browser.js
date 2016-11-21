50281c2b2874c654fd4da00d8f5732b8753e45ba
Marijn Haverbeke <marijn@haverbeke.nl>
2016-11-21 11:42:54 +0100

    Don't access navigator unless it's available
    Closes prosemirror/prosemirror-view#4

--- src/browser.js
+++ src/browser.js
@@ -1,10 +1,12 @@
-const ie_upto10 = /MSIE \d/.test(navigator.userAgent)
-const ie_11up = /Trident\/(?:[7-9]|\d{2,})\..*rv:(\d+)/.exec(navigator.userAgent)
+const result = module.exports = {}
 
-module.exports = {
-  mac: /Mac/.test(navigator.platform),
-  ie: ie_upto10 || !!ie_11up,
-  ie_version: ie_upto10 ? document.documentMode || 6 : ie_11up && +ie_11up[1],
-  gecko: /gecko\/\d/i.test(navigator.userAgent),
-  ios: /AppleWebKit/.test(navigator.userAgent) && /Mobile\/\w+/.test(navigator.userAgent)
+if (typeof navigator != "undefined") {
+  const ie_upto10 = /MSIE \d/.test(navigator.userAgent)
+  const ie_11up = /Trident\/(?:[7-9]|\d{2,})\..*rv:(\d+)/.exec(navigator.userAgent)
+
+  result.mac = /Mac/.test(navigator.platform)
+  result.ie = ie_upto10 || !!ie_11up
+  result.ie_version = ie_upto10 ? document.documentMode || 6 : ie_11up && +ie_11up[1]
+  result.gecko = /gecko\/\d/i.test(navigator.userAgent)
+  result.ios = /AppleWebKit/.test(navigator.userAgent) && /Mobile\/\w+/.test(navigator.userAgent)
 }
