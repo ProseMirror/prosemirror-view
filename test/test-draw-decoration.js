7b69bcd807a24bdc4a437edbd28793a453a5585c
Marijn Haverbeke <marijn@haverbeke.nl>
2016-12-16 23:22:24 +0100

    Simplify describe scopes in tests

--- test/test-draw-decoration.js
+++ test/test-draw-decoration.js
@@ -35,225 +35,223 @@ function decoPlugin(decos) {
   })
 }
 
-describe("EditorView", () => {
-  describe("draw", () => {
-    it("draws inline decorations", () => {
-      let view = tempEditor({doc: doc(p("foobar")),
-                             plugins: [decoPlugin(["2-5-foo"])]})
-      let found = view.content.querySelector(".foo")
-      ist(found)
-      ist(found.textContent, "oob")
-    })
+describe("Decoration drawing", () => {
+  it("draws inline decorations", () => {
+    let view = tempEditor({doc: doc(p("foobar")),
+                           plugins: [decoPlugin(["2-5-foo"])]})
+    let found = view.content.querySelector(".foo")
+    ist(found)
+    ist(found.textContent, "oob")
+  })
 
-    it("draws wrapping decorations", () => {
-      let view = tempEditor({doc: doc(p("foo")),
-                             plugins: [decoPlugin([Decoration.inline(1, 5, {nodeName: "i"})])]})
-      let found = view.content.querySelector("i")
-      ist(found && found.innerHTML, "foo")
-    })
+  it("draws wrapping decorations", () => {
+    let view = tempEditor({doc: doc(p("foo")),
+                           plugins: [decoPlugin([Decoration.inline(1, 5, {nodeName: "i"})])]})
+    let found = view.content.querySelector("i")
+    ist(found && found.innerHTML, "foo")
+  })
 
-    it("draws node decorations", () => {
-      let view = tempEditor({doc: doc(p("foo"), p("bar")),
-                             plugins: [decoPlugin([Decoration.node(5, 10, {class: "cls"})])]})
-      let found = view.content.querySelectorAll(".cls")
-      ist(found.length, 1)
-      ist(found[0].nodeName, "P")
-      ist(found[0].previousSibling.nodeName, "P")
-    })
+  it("draws node decorations", () => {
+    let view = tempEditor({doc: doc(p("foo"), p("bar")),
+                           plugins: [decoPlugin([Decoration.node(5, 10, {class: "cls"})])]})
+    let found = view.content.querySelectorAll(".cls")
+    ist(found.length, 1)
+    ist(found[0].nodeName, "P")
+    ist(found[0].previousSibling.nodeName, "P")
+  })
 
-    it("draws overlapping inline decorations", () => {
-      let view = tempEditor({doc: doc(p("abcdef")),
-                             plugins: [decoPlugin(["3-5-foo", "4-6-bar", "1-7-baz"])]})
-      let baz = view.content.querySelectorAll(".baz")
-      ist(baz.length, 5)
-      ist(Array.prototype.map.call(baz, x => x.textContent).join("-"), "ab-c-d-e-f")
-      function classes(n) { return n.className.split(" ").sort().join(" ") }
-      ist(classes(baz[1]), "baz foo")
-      ist(classes(baz[2]), "bar baz foo")
-      ist(classes(baz[3]), "bar baz")
-    })
+  it("draws overlapping inline decorations", () => {
+    let view = tempEditor({doc: doc(p("abcdef")),
+                           plugins: [decoPlugin(["3-5-foo", "4-6-bar", "1-7-baz"])]})
+    let baz = view.content.querySelectorAll(".baz")
+    ist(baz.length, 5)
+    ist(Array.prototype.map.call(baz, x => x.textContent).join("-"), "ab-c-d-e-f")
+    function classes(n) { return n.className.split(" ").sort().join(" ") }
+    ist(classes(baz[1]), "baz foo")
+    ist(classes(baz[2]), "bar baz foo")
+    ist(classes(baz[3]), "bar baz")
+  })
 
-    it("draws multiple widgets", () => {
-      let view = tempEditor({doc: doc(p("foobar")),
-                             plugins: [decoPlugin(["1-widget", "4-widget", "7-widget"])]})
-      let found = view.content.querySelectorAll("button")
-      ist(found.length, 3)
-      ist(found[0].nextSibling.textContent, "foo")
-      ist(found[1].nextSibling.textContent, "bar")
-      ist(found[2].previousSibling.textContent, "bar")
-    })
+  it("draws multiple widgets", () => {
+    let view = tempEditor({doc: doc(p("foobar")),
+                           plugins: [decoPlugin(["1-widget", "4-widget", "7-widget"])]})
+    let found = view.content.querySelectorAll("button")
+    ist(found.length, 3)
+    ist(found[0].nextSibling.textContent, "foo")
+    ist(found[1].nextSibling.textContent, "bar")
+    ist(found[2].previousSibling.textContent, "bar")
+  })
 
-    it("draws a widget in an empty node", () => {
-      let view = tempEditor({doc: doc(p()),
-                             plugins: [decoPlugin(["1-widget"])]})
-      ist(view.content.querySelectorAll("button").length, 1)
-    })
+  it("draws a widget in an empty node", () => {
+    let view = tempEditor({doc: doc(p()),
+                           plugins: [decoPlugin(["1-widget"])]})
+    ist(view.content.querySelectorAll("button").length, 1)
+  })
 
-    it("draws widgets on node boundaries", () => {
-      let view = tempEditor({doc: doc(p("foo", em("bar"))),
-                             plugins: [decoPlugin(["4-widget"])]})
-      ist(view.content.querySelectorAll("button").length, 1)
-    })
+  it("draws widgets on node boundaries", () => {
+    let view = tempEditor({doc: doc(p("foo", em("bar"))),
+                           plugins: [decoPlugin(["4-widget"])]})
+    ist(view.content.querySelectorAll("button").length, 1)
+  })
 
-    it("draws decorations from multiple plugins", () => {
-      let view = tempEditor({doc: doc(p("foo", em("bar"))),
-                             plugins: [decoPlugin(["2-widget"]), decoPlugin(["6-widget"])]})
-      ist(view.content.querySelectorAll("button").length, 2)
-    })
+  it("draws decorations from multiple plugins", () => {
+    let view = tempEditor({doc: doc(p("foo", em("bar"))),
+                           plugins: [decoPlugin(["2-widget"]), decoPlugin(["6-widget"])]})
+    ist(view.content.querySelectorAll("button").length, 2)
+  })
 
-    it("draws inline decorations spanning multiple parents", () => {
-      let view = tempEditor({doc: doc(p("long first ", em("p"), "aragraph"), p("two")),
-                             plugins: [decoPlugin(["7-25-foo"])]})
-      let foos = view.content.querySelectorAll(".foo")
-      ist(foos.length, 4)
-      ist(foos[0].textContent, "irst ")
-      ist(foos[1].textContent, "p")
-      ist(foos[2].textContent, "aragraph")
-      ist(foos[3].textContent, "tw")
-    })
+  it("draws inline decorations spanning multiple parents", () => {
+    let view = tempEditor({doc: doc(p("long first ", em("p"), "aragraph"), p("two")),
+                           plugins: [decoPlugin(["7-25-foo"])]})
+    let foos = view.content.querySelectorAll(".foo")
+    ist(foos.length, 4)
+    ist(foos[0].textContent, "irst ")
+    ist(foos[1].textContent, "p")
+    ist(foos[2].textContent, "aragraph")
+    ist(foos[3].textContent, "tw")
+  })
 
-    it("draws inline decorations across empty paragraphs", () => {
-      let view = tempEditor({doc: doc(p("first"), p(), p("second")),
-                             plugins: [decoPlugin(["3-12-foo"])]})
-      let foos = view.content.querySelectorAll(".foo")
-      ist(foos.length, 2)
-      ist(foos[0].textContent, "rst")
-      ist(foos[1].textContent, "se")
-    })
+  it("draws inline decorations across empty paragraphs", () => {
+    let view = tempEditor({doc: doc(p("first"), p(), p("second")),
+                           plugins: [decoPlugin(["3-12-foo"])]})
+    let foos = view.content.querySelectorAll(".foo")
+    ist(foos.length, 2)
+    ist(foos[0].textContent, "rst")
+    ist(foos[1].textContent, "se")
+  })
 
-    it("can handle inline decorations ending at the start or end of a node", () => {
-      let view = tempEditor({doc: doc(p(), p()),
-                             plugins: [decoPlugin(["1-3-foo"])]})
-      ist(!view.content.querySelector(".foo"))
-    })
+  it("can handle inline decorations ending at the start or end of a node", () => {
+    let view = tempEditor({doc: doc(p(), p()),
+                           plugins: [decoPlugin(["1-3-foo"])]})
+    ist(!view.content.querySelector(".foo"))
+  })
 
-    it("can draw decorations with multiple classes", () => {
-      let view = tempEditor({doc: doc(p("foo")),
-                             plugins: [decoPlugin(["1-4-foo bar"])]})
-      ist(view.content.querySelectorAll(".foo").length, 1)
-      ist(view.content.querySelectorAll(".bar").length, 1)
-    })
+  it("can draw decorations with multiple classes", () => {
+    let view = tempEditor({doc: doc(p("foo")),
+                           plugins: [decoPlugin(["1-4-foo bar"])]})
+    ist(view.content.querySelectorAll(".foo").length, 1)
+    ist(view.content.querySelectorAll(".bar").length, 1)
+  })
 
-    it("supports overlapping inline decorations", () => {
-      let view = tempEditor({doc: doc(p("foobar")),
-                             plugins: [decoPlugin(["1-3-foo", "2-5-bar"])]})
-      let foos = view.content.querySelectorAll(".foo")
-      let bars = view.content.querySelectorAll(".bar")
-      ist(foos.length, 2)
-      ist(bars.length, 2)
-      ist(foos[0].textContent, "f")
-      ist(foos[1].textContent, "o")
-      ist(bars[0].textContent, "o")
-      ist(bars[1].textContent, "ob")
-    })
+  it("supports overlapping inline decorations", () => {
+    let view = tempEditor({doc: doc(p("foobar")),
+                           plugins: [decoPlugin(["1-3-foo", "2-5-bar"])]})
+    let foos = view.content.querySelectorAll(".foo")
+    let bars = view.content.querySelectorAll(".bar")
+    ist(foos.length, 2)
+    ist(bars.length, 2)
+    ist(foos[0].textContent, "f")
+    ist(foos[1].textContent, "o")
+    ist(bars[0].textContent, "o")
+    ist(bars[1].textContent, "ob")
+  })
 
-    it("doesn't redraw when irrelevant decorations change", () => {
-      let view = tempEditor({doc: doc(p("foo"), p("baz")),
-                             plugins: [decoPlugin(["7-8-foo"])]})
-      let para2 = view.content.lastChild
-      view.props.onAction({type: "changeDecorations", add: [make("2-3-bar")]})
-      ist(view.content.lastChild, para2)
-      ist(view.content.querySelector(".bar"))
-    })
+  it("doesn't redraw when irrelevant decorations change", () => {
+    let view = tempEditor({doc: doc(p("foo"), p("baz")),
+                           plugins: [decoPlugin(["7-8-foo"])]})
+    let para2 = view.content.lastChild
+    view.props.onAction({type: "changeDecorations", add: [make("2-3-bar")]})
+    ist(view.content.lastChild, para2)
+    ist(view.content.querySelector(".bar"))
+  })
 
-    it("doesn't redraw when irrelevant content changes", () => {
-      let view = tempEditor({doc: doc(p("foo"), p("baz")),
-                             plugins: [decoPlugin(["7-8-foo"])]})
-      let para2 = view.content.lastChild
-      view.props.onAction(view.state.tr.delete(2, 3).action())
-      view.props.onAction(view.state.tr.delete(2, 3).action())
-      ist(view.content.lastChild, para2)
-    })
+  it("doesn't redraw when irrelevant content changes", () => {
+    let view = tempEditor({doc: doc(p("foo"), p("baz")),
+                           plugins: [decoPlugin(["7-8-foo"])]})
+    let para2 = view.content.lastChild
+    view.props.onAction(view.state.tr.delete(2, 3).action())
+    view.props.onAction(view.state.tr.delete(2, 3).action())
+    ist(view.content.lastChild, para2)
+  })
 
-    it("can add a widget on a node boundary", () => {
-      let view = tempEditor({doc: doc(p("foo", em("bar"))),
-                             plugins: [decoPlugin([])]})
-      view.props.onAction({type: "changeDecorations", add: [make("4-widget")]})
-      ist(view.content.querySelectorAll("button").length, 1)
-    })
+  it("can add a widget on a node boundary", () => {
+    let view = tempEditor({doc: doc(p("foo", em("bar"))),
+                           plugins: [decoPlugin([])]})
+    view.props.onAction({type: "changeDecorations", add: [make("4-widget")]})
+    ist(view.content.querySelectorAll("button").length, 1)
+  })
 
-    it("can remove a widget on a node boundary", () => {
-      let dec = make("4-widget")
-      let view = tempEditor({doc: doc(p("foo", em("bar"))),
-                             plugins: [decoPlugin([dec])]})
-      view.props.onAction({type: "changeDecorations", remove: [dec]})
-      ist(view.content.querySelector("button"), null)
-    })
+  it("can remove a widget on a node boundary", () => {
+    let dec = make("4-widget")
+    let view = tempEditor({doc: doc(p("foo", em("bar"))),
+                           plugins: [decoPlugin([dec])]})
+    view.props.onAction({type: "changeDecorations", remove: [dec]})
+    ist(view.content.querySelector("button"), null)
+  })
 
-    it("can remove the class from a text node", () => {
-      let dec = make("1-4-foo")
-      let view = tempEditor({doc: doc(p("abc")),
-                             plugins: [decoPlugin([dec])]})
-      ist(view.content.querySelector(".foo"))
-      view.props.onAction({type: "changeDecorations", remove: [dec]})
-      ist(view.content.querySelector(".foo"), null)
-    })
+  it("can remove the class from a text node", () => {
+    let dec = make("1-4-foo")
+    let view = tempEditor({doc: doc(p("abc")),
+                           plugins: [decoPlugin([dec])]})
+    ist(view.content.querySelector(".foo"))
+    view.props.onAction({type: "changeDecorations", remove: [dec]})
+    ist(view.content.querySelector(".foo"), null)
+  })
 
-    it("can remove the class from part of a text node", () => {
-      let dec = make("2-4-foo")
-      let view = tempEditor({doc: doc(p("abcd")),
-                             plugins: [decoPlugin([dec])]})
-      ist(view.content.querySelector(".foo"))
-      view.props.onAction({type: "changeDecorations", remove: [dec]})
-      ist(view.content.querySelector(".foo"), null)
-      ist(view.content.firstChild.innerHTML, "abcd")
-    })
+  it("can remove the class from part of a text node", () => {
+    let dec = make("2-4-foo")
+    let view = tempEditor({doc: doc(p("abcd")),
+                           plugins: [decoPlugin([dec])]})
+    ist(view.content.querySelector(".foo"))
+    view.props.onAction({type: "changeDecorations", remove: [dec]})
+    ist(view.content.querySelector(".foo"), null)
+    ist(view.content.firstChild.innerHTML, "abcd")
+  })
 
-    it("can remove the class for part of a text node", () => {
-      let dec = make("2-4-foo")
-      let view = tempEditor({doc: doc(p("abcd")),
-                             plugins: [decoPlugin([dec])]})
-      ist(view.content.querySelector(".foo"))
-      view.props.onAction({type: "changeDecorations", remove: [dec], add: [make("2-4-bar")]})
-      ist(view.content.querySelector(".foo"), null)
-      ist(view.content.querySelector(".bar"))
-    })
+  it("can remove the class for part of a text node", () => {
+    let dec = make("2-4-foo")
+    let view = tempEditor({doc: doc(p("abcd")),
+                           plugins: [decoPlugin([dec])]})
+    ist(view.content.querySelector(".foo"))
+    view.props.onAction({type: "changeDecorations", remove: [dec], add: [make("2-4-bar")]})
+    ist(view.content.querySelector(".foo"), null)
+    ist(view.content.querySelector(".bar"))
+  })
 
-    it("draws a widget added in the middle of a text node", () => {
-      let view = tempEditor({doc: doc(p("foo")), plugins: [decoPlugin([])]})
-      view.props.onAction({type: "changeDecorations", add: [make("3-widget")]})
-      ist(view.content.firstChild.textContent, "foωo")
-    })
+  it("draws a widget added in the middle of a text node", () => {
+    let view = tempEditor({doc: doc(p("foo")), plugins: [decoPlugin([])]})
+    view.props.onAction({type: "changeDecorations", add: [make("3-widget")]})
+    ist(view.content.firstChild.textContent, "foωo")
+  })
 
-    it("can update a text node around a widget", () => {
-      let view = tempEditor({doc: doc(p("bar")), plugins: [decoPlugin(["3-widget"])]})
-      view.props.onAction(view.state.tr.delete(1, 2).action())
-      ist(view.content.querySelectorAll("button").length, 1)
-      ist(view.content.firstChild.textContent, "aωr")
-    })
+  it("can update a text node around a widget", () => {
+    let view = tempEditor({doc: doc(p("bar")), plugins: [decoPlugin(["3-widget"])]})
+    view.props.onAction(view.state.tr.delete(1, 2).action())
+    ist(view.content.querySelectorAll("button").length, 1)
+    ist(view.content.firstChild.textContent, "aωr")
+  })
 
-    it("can update a text node with an inline decoration", () => {
-      let view = tempEditor({doc: doc(p("bar")), plugins: [decoPlugin(["1-3-foo"])]})
-      view.props.onAction(view.state.tr.delete(1, 2).action())
-      let foo = view.content.querySelector(".foo")
-      ist(foo)
-      ist(foo.textContent, "a")
-      ist(foo.nextSibling.textContent, "r")
-    })
+  it("can update a text node with an inline decoration", () => {
+    let view = tempEditor({doc: doc(p("bar")), plugins: [decoPlugin(["1-3-foo"])]})
+    view.props.onAction(view.state.tr.delete(1, 2).action())
+    let foo = view.content.querySelector(".foo")
+    ist(foo)
+    ist(foo.textContent, "a")
+    ist(foo.nextSibling.textContent, "r")
+  })
 
-    it("correctly redraws a partially decorated node when a widget is added", () => {
-      let view = tempEditor({doc: doc(p("one", em("two"))),
-                             plugins: [decoPlugin(["1-6-foo"])]})
-      view.props.onAction({type: "changeDecorations", add: [make("6-widget")]})
-      let foos = view.content.querySelectorAll(".foo")
-      ist(foos.length, 2)
-      ist(foos[0].textContent, "one")
-      ist(foos[1].textContent, "tw")
-    })
+  it("correctly redraws a partially decorated node when a widget is added", () => {
+    let view = tempEditor({doc: doc(p("one", em("two"))),
+                           plugins: [decoPlugin(["1-6-foo"])]})
+    view.props.onAction({type: "changeDecorations", add: [make("6-widget")]})
+    let foos = view.content.querySelectorAll(".foo")
+    ist(foos.length, 2)
+    ist(foos[0].textContent, "one")
+    ist(foos[1].textContent, "tw")
+  })
 
-    it("correctly redraws when skipping split text node", () => {
-      let view = tempEditor({doc: doc(p("foo")),
-                             plugins: [decoPlugin(["3-widget", "3-4-foo"])]})
-      view.props.onAction({type: "changeDecorations", add: [make("4-widget")]})
-      ist(view.content.querySelectorAll("button").length, 2)
-    })
+  it("correctly redraws when skipping split text node", () => {
+    let view = tempEditor({doc: doc(p("foo")),
+                           plugins: [decoPlugin(["3-widget", "3-4-foo"])]})
+    view.props.onAction({type: "changeDecorations", add: [make("4-widget")]})
+    ist(view.content.querySelectorAll("button").length, 2)
+  })
 
-    it("drops removed node decorations from the view", () => {
-      let deco = Decoration.node(1, 6, {class: "cls"})
-      let view = tempEditor({doc: doc(blockquote(p("foo"), p("bar"))),
-                             plugins: [decoPlugin([deco])]})
-      view.props.onAction({type: "changeDecorations", remove: [deco]})
-      ist(!view.content.querySelector(".cls"))
-    })
+  it("drops removed node decorations from the view", () => {
+    let deco = Decoration.node(1, 6, {class: "cls"})
+    let view = tempEditor({doc: doc(blockquote(p("foo"), p("bar"))),
+                           plugins: [decoPlugin([deco])]})
+    view.props.onAction({type: "changeDecorations", remove: [deco]})
+    ist(!view.content.querySelector(".cls"))
   })
 })
--- test/test-draw.js
+++ test/test-draw.js
@@ -6,53 +6,51 @@ function apply(view, tr) {
   view.props.onAction(tr.action())
 }
 
-describe("EditorView", () => {
-  describe("draw", () => {
-    it("updates the DOM", () => {
-      let view = tempEditor({doc: doc(p("foo"))})
-      apply(view, view.state.tr.insertText("bar"))
-      ist(view.content.textContent, "barfoo")
-    })
+describe("EditorView edraw", () => {
+  it("updates the DOM", () => {
+    let view = tempEditor({doc: doc(p("foo"))})
+    apply(view, view.state.tr.insertText("bar"))
+    ist(view.content.textContent, "barfoo")
+  })
 
-    it("doesn't redraw nodes after changes", () => {
-      let view = tempEditor({doc: doc(h1("foo<a>"), p("bar"))})
-      let oldP = view.content.querySelector("p")
-      apply(view, view.state.tr.insertText("!"))
-      ist(view.content.querySelector("p"), oldP)
-    })
+  it("doesn't redraw nodes after changes", () => {
+    let view = tempEditor({doc: doc(h1("foo<a>"), p("bar"))})
+    let oldP = view.content.querySelector("p")
+    apply(view, view.state.tr.insertText("!"))
+    ist(view.content.querySelector("p"), oldP)
+  })
 
-    it("doesn't redraw nodes before changes", () => {
-      let view = tempEditor({doc: doc(p("foo"), h1("bar"))})
-      let oldP = view.content.querySelector("p")
-      apply(view, view.state.tr.insertText("!", 2))
-      ist(view.content.querySelector("p"), oldP)
-    })
+  it("doesn't redraw nodes before changes", () => {
+    let view = tempEditor({doc: doc(p("foo"), h1("bar"))})
+    let oldP = view.content.querySelector("p")
+    apply(view, view.state.tr.insertText("!", 2))
+    ist(view.content.querySelector("p"), oldP)
+  })
 
-    it("doesn't redraw nodes between changes", () => {
-      let view = tempEditor({doc: doc(p("foo"), h1("bar"), pre("baz"))})
-      let oldP = view.content.querySelector("p")
-      let oldPre = view.content.querySelector("pre")
-      apply(view, view.state.tr.insertText("!", 2))
-      ist(view.content.querySelector("p"), oldP)
-      ist(view.content.querySelector("pre"), oldPre)
-    })
+  it("doesn't redraw nodes between changes", () => {
+    let view = tempEditor({doc: doc(p("foo"), h1("bar"), pre("baz"))})
+    let oldP = view.content.querySelector("p")
+    let oldPre = view.content.querySelector("pre")
+    apply(view, view.state.tr.insertText("!", 2))
+    ist(view.content.querySelector("p"), oldP)
+    ist(view.content.querySelector("pre"), oldPre)
+  })
 
-    it("doesn't redraw siblings of a split node", () => {
-      let view = tempEditor({doc: doc(p("foo"), h1("bar"), pre("baz"))})
-      let oldP = view.content.querySelector("p")
-      let oldPre = view.content.querySelector("pre")
-      apply(view, view.state.tr.split(8))
-      ist(view.content.querySelector("p"), oldP)
-      ist(view.content.querySelector("pre"), oldPre)
-    })
+  it("doesn't redraw siblings of a split node", () => {
+    let view = tempEditor({doc: doc(p("foo"), h1("bar"), pre("baz"))})
+    let oldP = view.content.querySelector("p")
+    let oldPre = view.content.querySelector("pre")
+    apply(view, view.state.tr.split(8))
+    ist(view.content.querySelector("p"), oldP)
+    ist(view.content.querySelector("pre"), oldPre)
+  })
 
-    it("doesn't redraw siblings of a joined node", () => {
-      let view = tempEditor({doc: doc(p("foo"), h1("bar"), h1("x"), pre("baz"))})
-      let oldP = view.content.querySelector("p")
-      let oldPre = view.content.querySelector("pre")
-      apply(view, view.state.tr.join(10))
-      ist(view.content.querySelector("p"), oldP)
-      ist(view.content.querySelector("pre"), oldPre)
-    })
+  it("doesn't redraw siblings of a joined node", () => {
+    let view = tempEditor({doc: doc(p("foo"), h1("bar"), h1("x"), pre("baz"))})
+    let oldP = view.content.querySelector("p")
+    let oldPre = view.content.querySelector("pre")
+    apply(view, view.state.tr.join(10))
+    ist(view.content.querySelector("p"), oldP)
+    ist(view.content.querySelector("pre"), oldPre)
   })
 })
