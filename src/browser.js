2b4b1f49ead4904beeaae33e7837ee98cd5af1b6
Marijn Haverbeke <marijn@haverbeke.nl>
2017-03-31 18:15:16 +0200

    Fix browser detection for increasingly ridiculous Edge userAgent

--- src/browser.js
+++ src/browser.js
@@ -1,13 +1,14 @@
 const result = module.exports = {}
 
 if (typeof navigator != "undefined") {
+  const ie_edge = /Edge\/(\d+)/.exec(navigator.userAgent)
   const ie_upto10 = /MSIE \d/.test(navigator.userAgent)
   const ie_11up = /Trident\/(?:[7-9]|\d{2,})\..*rv:(\d+)/.exec(navigator.userAgent)
 
   result.mac = /Mac/.test(navigator.platform)
-  result.ie = ie_upto10 || !!ie_11up
-  result.ie_version = ie_upto10 ? document.documentMode || 6 : ie_11up && +ie_11up[1]
-  result.gecko = /gecko\/\d/i.test(navigator.userAgent)
-  result.ios = /AppleWebKit/.test(navigator.userAgent) && /Mobile\/\w+/.test(navigator.userAgent)
-  result.webkit = 'WebkitAppearance' in document.documentElement.style
+  let ie = result.ie = !!(ie_upto10 || ie_11up || ie_edge)
+  result.ie_version = ie_upto10 ? document.documentMode || 6 : ie_11up ? +ie_11up[1] : ie_edge ? +ie_edge[1] : null
+  result.gecko = !ie && /gecko\/\d/i.test(navigator.userAgent)
+  result.ios = !ie && /AppleWebKit/.test(navigator.userAgent) && /Mobile\/\w+/.test(navigator.userAgent)
+  result.webkit = !ie && 'WebkitAppearance' in document.documentElement.style
 }
