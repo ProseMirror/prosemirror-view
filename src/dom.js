d5e3fdd4bcaf26031fa7ab4edfee9349b6fd358c
Marijn Haverbeke <marijn@haverbeke.nl>
2018-08-24 17:13:14 +0200

    Don't exit non-editable nodes when scanning for equivalent selections
    FIX: Improves selection management around widgets with no actual
    HTML content (possibly drawn using CSS pseudo elements).
    
    Issue prosemirror/prosemirror#832

--- src/dom.js
+++ src/dom.js
@@ -34,7 +34,8 @@ function scanFor(node, off, targetNode, targetOff, dir) {
     if (node == targetNode && off == targetOff) return true
     if (off == (dir < 0 ? 0 : nodeSize(node)) || node.nodeType == 3 && node.nodeValue == "\ufeff") {
       let parent = node.parentNode
-      if (parent.nodeType != 1 || hasBlockDesc(node) || atomElements.test(node.nodeName)) return false
+      if (parent.nodeType != 1 || hasBlockDesc(node) || atomElements.test(node.nodeName) || node.contentEditable == "false")
+        return false
       off = domIndex(node) + (dir < 0 ? 0 : 1)
       node = parent
     } else if (node.nodeType == 1) {
