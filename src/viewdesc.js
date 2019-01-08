2896c2898f6adf64080e016cd75bb9df4af2277f
Eduard Shvedai <eshvedai@atlassian.com>
2019-01-09 10:21:02 +1100

    changed the approach to use mark spec instead of nodeviews

--- src/viewdesc.js
+++ src/viewdesc.js
@@ -471,10 +471,9 @@ class CursorWrapperDesc extends WidgetViewDesc {
 // necessary.
 class MarkViewDesc extends ViewDesc {
   // : (ViewDesc, Mark, dom.Node)
-  constructor(parent, mark, dom, contentDOM, disableWrapping) {
+  constructor(parent, mark, dom, contentDOM) {
     super(parent, [], dom, contentDOM)
     this.mark = mark
-    this.disableWrapping = disableWrapping
   }
 
   static create(parent, mark, inline, view) {
@@ -482,14 +481,13 @@ class MarkViewDesc extends ViewDesc {
     let spec = custom && custom(mark, view, inline)
     if (!spec || !spec.dom)
       spec = DOMSerializer.renderSpec(document, mark.type.spec.toDOM(mark, inline))
-    return new MarkViewDesc(parent, mark, spec.dom, spec.contentDOM || spec.dom, spec.disableWrapping)
+    return new MarkViewDesc(parent, mark, spec.dom, spec.contentDOM || spec.dom)
   }
 
   parseRule() { return {mark: this.mark.type.name, attrs: this.mark.attrs, contentElement: this.contentDOM} }
 
   matchesMark(mark) { 
-    let disableWrapping = this.contentDOM && this.disableWrapping
-    return this.dirty != NODE_DIRTY && !disableWrapping && this.mark.eq(mark) 
+    return this.dirty != NODE_DIRTY && mark.type.spec.spanning !== false && this.mark.eq(mark) 
   }
 
   markDirty(from, to) {
