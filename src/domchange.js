c699ec18f8aedecb13f2b665f59341b0626c23e6
Marijn Haverbeke <marijn@haverbeke.nl>
2019-10-15 14:02:59 +0200

    Work around Chrome Android issue
    FIX: Enabling a mark and then starting a composition, on Chrome Android, will
    no longer cause the cursor to jump to the start of the composition.
    
    See https://discuss.prosemirror.net/t/potential-alternatives-for-cursorwrapper/2264

--- src/domchange.js
+++ src/domchange.js
@@ -195,7 +195,11 @@ export function readDOMChange(view, from, to, typeOver) {
     tr = view.state.tr.replace(chFrom, chTo, parse.doc.slice(change.start - parse.from, change.endB - parse.from))
   if (parse.sel) {
     let sel = resolveSelection(view, tr.doc, parse.sel)
-    if (sel) tr.setSelection(sel)
+    // Chrome Android will sometimes, during composition, report the
+    // selection in the wrong place. If it looks like that is
+    // happening, don't update the selection.
+    if (sel && !(browser.chrome && browser.android && view.composing && sel.empty && sel.head == chFrom))
+      tr.setSelection(sel)
   }
   if (storedMarks) tr.ensureMarks(storedMarks)
   view.dispatch(tr.scrollIntoView())
