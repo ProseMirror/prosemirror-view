9d627e3ee1891b7d1f22a74c254c54486d0a009f
Marijn Haverbeke <marijn@haverbeke.nl>
2019-10-21 08:07:32 +0200

    Record old attribut values in DOM observer
    So that ignoreMutation methods have access to them
    
    FEATURE: The mutation records passed to
    [`ignoreMutation`](##view.NodeView.ignoreMutation) now contain the old
    attribute value.
    
    Issue prosemirror/prosemirror#988

--- src/domobserver.js
+++ src/domobserver.js
@@ -2,7 +2,14 @@ import browser from "./browser"
 import {domIndex, isEquivalentPosition} from "./dom"
 import {hasFocusAndSelection, hasSelection, selectionToDOM} from "./selection"
 
-const observeOptions = {childList: true, characterData: true, attributes: true, subtree: true, characterDataOldValue: true}
+const observeOptions = {
+  childList: true,
+  characterData: true,
+  characterDataOldValue: true,
+  attributes: true,
+  attributeOldValue: true,
+  subtree: true
+}
 // IE11 has very broken mutation observers, so we also listen to DOMCharacterDataModified
 const useCharData = browser.ie && browser.ie_version <= 11
 
