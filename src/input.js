ProseMirror/prosemirror-view

commit 1f9f136bfb8501057cce1135874f8c2bf8c2509a

Don't call handlePaste multiple times
FIX: Fix issue where the `handlePaste` prop might be called multiple times
for a single paste.

src/input.js
@@ -502,21 +502,22 @@ function capturePaste(view, e) {
 
 function doPaste(view, text, html, e) {
   let slice = parseFromClipboard(view, text, html, view.shiftKey, view.state.selection.$from)
-  if (view.someProp("handlePaste", f => f(view, e, slice || Slice.empty))) return true
-  if (!slice) return false
+  if (view.someProp("handlePaste", f => f(view, e, slice || Slice.empty)) || !slice) return
 
   let singleNode = sliceSingleNode(slice)
   let tr = singleNode ? view.state.tr.replaceSelectionWith(singleNode, view.shiftKey) : view.state.tr.replaceSelection(slice)
   view.dispatch(tr.scrollIntoView().setMeta("paste", true).setMeta("uiEvent", "paste"))
-  return true
 }
 
 editHandlers.paste = (view, e) => {
   let data = brokenClipboardAPI ? null : e.clipboardData
-  if (data && (doPaste(view, data.getData("text/plain"), data.getData("text/html"), e) || data.files.length > 0))
+  let html = data && data.getData("text/html"), text = data && data.getData("text/plain")
+  if (data && (html || text || data.files.length)) {
+    doPaste(view, html, text, e)
     e.preventDefault()
-  else
+  } else {
     capturePaste(view, e)
+  }
 }
 
 class Dragging {

commit 440eb9eb40cdcc6cc5441c273e20dc794486f69b

Fix bug introduced in 1f9f136bfb85010

src/input.js
@@ -513,7 +513,7 @@ editHandlers.paste = (view, e) => {
   let data = brokenClipboardAPI ? null : e.clipboardData
   let html = data && data.getData("text/html"), text = data && data.getData("text/plain")
   if (data && (html || text || data.files.length)) {
-    doPaste(view, html, text, e)
+    doPaste(view, text, html, e)
     e.preventDefault()
   } else {
     capturePaste(view, e)