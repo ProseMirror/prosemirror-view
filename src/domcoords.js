9af8ceac1ee60a30ced3611913db7f187bbb51ed
Ricardo Basto <38562688+rbasto1@users.noreply.github.com>
2021-03-07 22:48:19 +0100

    Fix scrolling when editor has css transform:scale()
    FIX: Fix the scrolling-into-view logic in the case where a scale transformation is applied to the editor.

--- src/domcoords.js
+++ src/domcoords.js
@@ -12,9 +12,12 @@ function getSide(value, side) {
 
 function clientRect(node) {
   let rect = node.getBoundingClientRect()
+  // Adjust for elements with style "transform: scale()"
+  let scaleX = (rect.width / node.offsetWidth) || 1
+  let scaleY = (rect.height / node.offsetHeight) || 1
   // Make sure scrollbar width isn't included in the rectangle
-  return {left: rect.left, right: rect.left + node.clientWidth,
-          top: rect.top, bottom: rect.top + node.clientHeight}
+  return {left: rect.left, right: rect.left + node.clientWidth * scaleX,
+          top: rect.top, bottom: rect.top + node.clientHeight * scaleY}
 }
 
 export function scrollRectIntoView(view, rect, startDOM) {
