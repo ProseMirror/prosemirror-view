ProseMirror/prosemirror-view

commit e2182cac368f355d319b1672b050a4100190e00a

Expand click-near-special-selection hack for Chrome to handle clicks near the selection
FIX: Fixes an issue where clicking near a node or other special selection on Chrome would
in some cases do nothing.

src/input.js
@@ -360,7 +360,7 @@ class MouseDown {
                // thus doesn't get a reaction from ProseMirror. This
                // works around that.
                (browser.chrome && !(this.view.state.selection instanceof TextSelection) &&
-                (pos.pos == this.view.state.selection.from || pos.pos == this.view.state.selection.to))) {
+                Math.min(Math.abs(pos.pos - this.view.state.selection.from), Math.abs(pos.pos - view.state.selection.to)) <= 2)) {
       updateSelection(this.view, Selection.near(this.view.state.doc.resolve(pos.pos)), "pointer")
       event.preventDefault()
     } else {

commit 1f0b81ae9ad367d1971feca76b664c9f2557a5eb

Fix a bug introduced in e2182cac368f355d319
FIX: Fix a crash in mouse click handling introduced in the previous version.

src/input.js
@@ -360,7 +360,8 @@ class MouseDown {
                // thus doesn't get a reaction from ProseMirror. This
                // works around that.
                (browser.chrome && !(this.view.state.selection instanceof TextSelection) &&
-                Math.min(Math.abs(pos.pos - this.view.state.selection.from), Math.abs(pos.pos - view.state.selection.to)) <= 2)) {
+                Math.min(Math.abs(pos.pos - this.view.state.selection.from),
+                         Math.abs(pos.pos - this.view.state.selection.to)) <= 2)) {
       updateSelection(this.view, Selection.near(this.view.state.doc.resolve(pos.pos)), "pointer")
       event.preventDefault()
     } else {