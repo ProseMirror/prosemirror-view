ProseMirror/prosemirror-view

commit 68cab14b2a92417317735d50f45ee9583ed9140b

Add an additional IMG element after an uneditable widget at end of line on Safari
To avoid a bug where the browser draws the cursor at the start of the line.

FIX: Work around a Safari bug where it draws the cursor at the start of the line
when it is after an uneditable node at the end of the line.

Issue prosemirror/prosemirror#1165

src/viewdesc.js
@@ -113,7 +113,7 @@ class ViewDesc {
   matchesWidget() { return false }
   matchesMark() { return false }
   matchesNode() { return false }
-  matchesHack() { return false }
+  matchesHack(_nodeName) { return false }
 
   get beforePosition() { return false }
 
@@ -851,7 +851,7 @@ class TextViewDesc extends NodeViewDesc {
 // around contentEditable terribleness.
 class TrailingHackViewDesc extends ViewDesc {
   parseRule() { return {ignore: true} }
-  matchesHack() { return this.dirty == NOT_DIRTY }
+  matchesHack(nodeName) { return this.dirty == NOT_DIRTY && this.dom.nodeName == nodeName }
   get domAtom() { return true }
 }
 
@@ -1188,13 +1188,20 @@ class ViewTreeUpdater {
     if (!lastChild || // Empty textblock
         !(lastChild instanceof TextViewDesc) ||
         /\n$/.test(lastChild.node.text)) {
-      if (this.index < this.top.children.length && this.top.children[this.index].matchesHack()) {
-        this.index++
-      } else {
-        let dom = document.createElement("br")
-        this.top.children.splice(this.index++, 0, new TrailingHackViewDesc(this.top, nothing, dom, null))
-        this.changed = true
-      }
+      // Avoid a bug in Safari's cursor drawing (#1165)
+      if (browser.safari && lastChild.dom.contentEditable == "false")
+        this.addHackNode("IMG")
+      this.addHackNode("BR")
+    }
+  }
+
+  addHackNode(nodeName) {
+    if (this.index < this.top.children.length && this.top.children[this.index].matchesHack(nodeName)) {
+      this.index++
+    } else {
+      let dom = document.createElement(nodeName)
+      this.top.children.splice(this.index++, 0, new TrailingHackViewDesc(this.top, nothing, dom, null))
+      this.changed = true
     }
   }
 }

commit 334b6fc7fb0bf514fb1bc270e53215db917c832b

Fix a regression introduced in 68cab14
Issue https://github.com/ProseMirror/prosemirror/issues/1165

src/viewdesc.js
@@ -1203,7 +1203,7 @@ class ViewTreeUpdater {
         !(lastChild instanceof TextViewDesc) ||
         /\n$/.test(lastChild.node.text)) {
       // Avoid a bug in Safari's cursor drawing (#1165)
-      if (browser.safari && lastChild.dom.contentEditable == "false")
+      if (browser.safari && lastChild && lastChild.dom.contentEditable == "false")
         this.addHackNode("IMG")
       this.addHackNode("BR")
     }