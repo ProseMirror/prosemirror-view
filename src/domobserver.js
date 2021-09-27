d8c4bb1c238d814359a345e576fdfcaaa17aabd2
Martin Auswöger <martin@auswoeger.com>
2021-09-27 15:08:36 +0200

    Restore the white-space warning

--- src/domobserver.js
+++ src/domobserver.js
@@ -169,7 +169,10 @@ export class DOMObserver {
     }
 
     if (from > -1 || newSel) {
-      if (from > -1) this.view.docView.markDirty(from, to)
+      if (from > -1) {
+        this.view.docView.markDirty(from, to)
+        checkCSS(this.view)
+      }
       this.handleDOMChange(from, to, typeOver, added)
       if (this.view.docView.dirty) this.view.updateState(this.view.state)
       else if (!this.currentSelection.eq(sel)) selectionToDOM(this.view)
@@ -223,4 +226,13 @@ export class DOMObserver {
       }
     }
   }
+}
+
+let cssChecked = false
+
+function checkCSS(view) {
+  if (cssChecked) return
+  cssChecked = true
+  if (getComputedStyle(view.dom).whiteSpace == "normal")
+    console["warn"]("ProseMirror expects the CSS white-space property to be set, preferably to 'pre-wrap'. It is recommended to load style/prosemirror.css from the prosemirror-view package.")
 }
