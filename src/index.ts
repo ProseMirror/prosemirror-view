c0fb40d61237bd4cbd9b51f701a79624110dcb09
Marijn Haverbeke <marijn@haverbeke.berlin>
2023-05-10 08:11:46 +0200

    Allow user-specified attributes to override translate
    FIX: Make it possible for the `attributes` prop to override the `translate`
    attribute.
    
    Issue https://github.com/ProseMirror/prosemirror-view/pull/158

--- src/index.ts
+++ src/index.ts
@@ -478,7 +478,6 @@ function computeDocDeco(view: EditorView) {
   let attrs = Object.create(null)
   attrs.class = "ProseMirror"
   attrs.contenteditable = String(view.editable)
-  attrs.translate = "no"
 
   view.someProp("attributes", value => {
     if (typeof value == "function") value = value(view.state)
@@ -491,6 +490,7 @@ function computeDocDeco(view: EditorView) {
         attrs[attr] = String(value[attr])
     }
   })
+  if (!attrs.translate) attrs.translate = "no"
 
   return [Decoration.node(0, view.state.doc.content.size, attrs)]
 }
